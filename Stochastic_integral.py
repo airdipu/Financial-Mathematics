3e0c9454048067a6429f728283f157cf9041d5eb
airdipu <airdipu@gmail.com>
2021-09-22 08:07:00 +0300

    added updated stochastic integralwith simple process

--- Stochastic_integral.py
+++ Stochastic_integral.py
@@ -20,23 +20,23 @@ tk = random.sample(range(0, n), 3)
 ti = np.sort(tk)                # length of the interval for simple process
 
 k1 = ti[0]
-dW1 = np.random.normal(0,1,k1)*np.sqrt(1/k1)
+dW1 = np.random.normal(0,1,k1) * np.sqrt(1/k1)
 I1 = X[0] * (np.cumsum(dW1))
 
 k2 = ti[1]-ti[0]
-dW2 = np.random.normal(0,1,k2)*np.sqrt(1/k2)
+dW2 = np.random.normal(0,1,k2) * np.sqrt(1/k2)
 I2 = X[1] * (np.cumsum(dW2))
 
 S1 = np.append(I1,I2)
 
 k3 = ti[2]-ti[1]
-dW3 = np.random.normal(0,1,k3)*np.sqrt(1/k3)
+dW3 = np.random.normal(0,1,k3) * np.sqrt(1/k3)
 I3 = X[2] * (np.cumsum(dW3))
 
 S2 = np.append(S1, I3)
 
 k3 = n-ti[2]
-dW4 = np.random.normal(0,1,k3)*np.sqrt(1/k3)
+dW4 = np.random.normal(0,1,k3) * np.sqrt(1/k3)
 I4 = X[3] * (np.cumsum(dW4))
 
 S3 = np.append(S2, I4)
@@ -49,3 +49,4 @@ t = np.linspace(0, 1, num=n+1)
 
 plt.plot(t,I, 'r')
 plt.show()
+
