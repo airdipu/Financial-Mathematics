840edd544ab4c8d47bd63582a4ca8fecfdd734b9
airdipu <airdipu@gmail.com>
2021-09-22 08:09:58 +0300

    added updated stochastic integral with simple process

--- Stochastic_integral.py
+++ Stochastic_integral.py
@@ -12,7 +12,7 @@ import random
 
 # Simulation of a Stochastic integral with simple process
 
-n = 2000
+n = 1500
 
 W0 = 0                          # initial value of the Brownian motion
 X = np.random.uniform(-2,2,4)   # Generate uniformly distributed random heights of the step function 
@@ -23,29 +23,29 @@ k1 = ti[0]
 dW1 = np.random.normal(0,1,k1) * np.sqrt(1/k1)
 I1 = X[0] * (np.cumsum(dW1))
 
-k2 = ti[1]-ti[0]
+k2 = ti[1] - ti[0]
 dW2 = np.random.normal(0,1,k2) * np.sqrt(1/k2)
 I2 = X[1] * (np.cumsum(dW2))
 
-S1 = np.append(I1,I2)
+S1 = np.append(I1, I2)
 
-k3 = ti[2]-ti[1]
+k3 = ti[2] - ti[1]
 dW3 = np.random.normal(0,1,k3) * np.sqrt(1/k3)
 I3 = X[2] * (np.cumsum(dW3))
 
 S2 = np.append(S1, I3)
 
-k3 = n-ti[2]
+k3 = n - ti[2]
 dW4 = np.random.normal(0,1,k3) * np.sqrt(1/k3)
 I4 = X[3] * (np.cumsum(dW4))
 
 S3 = np.append(S2, I4)
 
-I = np.append(W0,S3)
+I = np.append(W0, S3)
 
 # Plot the path of the stochastic integral
 
-t = np.linspace(0, 1, num=n+1)
+t = np.linspace(0, 1, num = n+1)
 
 plt.plot(t,I, 'r')
 plt.show()
